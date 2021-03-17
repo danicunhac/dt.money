cab92c7e2a926babbab303129ecf4c18d2307377
Daniel Cunha <danielcc.jp@gmail.com>
2021-03-17 13:21:01 -0300

    style(add): variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import styled from "styled-components";
 import { GlobalStyle } from "./styles/global";
-
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 4rem;
-`;
 
 export const App = () => {
   return (
     <>
-      <Title>App</Title>
+      <h1>App</h1>
       <GlobalStyle />
     </>
   );
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,13 +1,49 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f2f5;
+    --red: #e52e4d;
+    --blue: #5429cc;
+
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --background: #f0f2f5;
+    --shape: #fff;
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box
   }
+  
+  html {
+    // 16px
+
+    @media(max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+
+    @media(max-width: 720px) {
+      font-size: 87.5%; // 14px
+    }
+  }
 
   body {
-    font: 1rem #444;
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 `;
