446527243c70c2dd2966c89de61b95c276abf6c9
Daniel Cunha <danielcc.jp@gmail.com>
2021-03-19 19:14:25 -0300

    feat: transactions table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from '../Summary';
+import { TransactionsTable } from '../TransactionsTable';
 import { Container } from './styles';
 
 function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 }
