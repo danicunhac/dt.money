170917f3b83a74b5878227bf336e4902dcae290a
Daniel Cunha <danielcc.jp@gmail.com>
2021-03-31 08:42:53 -0300

    feat: insert transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
 import React, { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -22,7 +23,9 @@ export function NewTransactionModal({
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log(title, value, category, type);
+    const data = { title, value, category, type };
+
+    api.post('/transactions', data);
   }
 
   return (
