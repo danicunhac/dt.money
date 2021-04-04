1e293ff551438be429f71798a4c74c7808d6bfa1
Daniel Cunha <danielcc.jp@gmail.com>
2021-04-04 12:16:09 -0300

    feat: context api

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
+import { TransactionsContext } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -21,7 +22,7 @@ export const App = () => {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -29,6 +30,6 @@ export const App = () => {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 };
